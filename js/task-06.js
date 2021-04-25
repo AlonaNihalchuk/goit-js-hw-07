a4485c477af635293f2b25a83f72bc5705843361
AlonaNihalchuk <acetnikopol@gmail.com>
2021-04-25 21:31:06 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,21 +2,50 @@ const validationInputRef = document.querySelector('#validation-input');
 
 validationInputRef.addEventListener('blur', onValidationInputBlur);
 
-function onValidationInput() {
-    validationInputRef.classList.add('valid');
-}
-
-function onInvalidationInput() {
-    validationInputRef.classList.toggle('invalid');
-}
-
 function onValidationInputBlur(event) {
-    if (event.currentTarget.value.length === 6) {
-        onValidationInput();
-    };
-    onInvalidationInput();
-    console.log(event.currentTarget.value.length);
+    if (event.currentTarget.value.length === Number(validationInputRef.dataset.length)) {
+        validationInputRef.classList.add('valid');
+        validationInputRef.classList.remove('invalid');
+        return;
+    }
+    validationInputRef.classList.add('invalid');
+    validationInputRef.classList.remove('valid');
+    return;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // 1. находим ссылку на input (const validationInputRef = document.querySelector('#validation-input');
 // 2. вешаем слушателя на input (validationInputRef.addEventListener('blur', onValidationInputBlur);
