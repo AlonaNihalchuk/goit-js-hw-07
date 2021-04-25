3953b2cbd2b89fa5577b73b2674b72073c55e64b
AlonaNihalchuk <acetnikopol@gmail.com>
2021-04-25 22:40:48 +0200

    task-06 changes

--- js/task-06.js
+++ js/task-06.js
@@ -44,23 +44,3 @@ function onValidationInputBlur(event) {
 
 
 
-
-
-
-// 1. находим ссылку на input (const validationInputRef = document.querySelector('#validation-input');
-// 2. вешаем слушателя на input (validationInputRef.addEventListener('blur', onValidationInputBlur);
-//    тут событие 'blur', и колбэк-функция
-// 3. делаем отдельные 2 функции: 1я функция добавляет класс 'valid' 
-//    function onValidationInput() {
-//    validationInputRef.classList.add('valid');}
-//    2я функция делает toggle класса 'invalid'(toggle убирает класс если он есть и добавляет если его нет) 
-//    function onInvalidationInput() {
-//     validationInputRef.classList.toggle('invalid');}
-// 4. функция callback с условием 
-//     function onValidationInputBlur(event) {
-//     if (event.currentTarget.value.length === 6) {
-//         onValidationInput();
-//     };
-//     onInvalidationInput();
-//     console.log(event.currentTarget.value.length);
-// }
